0f5e857c4431d7a4e068be8cc3e5e1002ec1841f
Nomsociuu <nomsociuu2004@gmail.com>
2025-10-03 04:41:00 +0700

    fix payment function

--- config/payos.js
+++ config/payos.js
@@ -1,11 +1,10 @@
-// config/payos.js
 const { PayOS } = require("@payos/node");
 require("dotenv").config();
 
-const payOs = new PayOS(
-  process.env.PAYOS_CLIENT_ID,
-  process.env.PAYOS_API_KEY,
-  process.env.PAYOS_CHECKSUM_KEY
-);
+const payOs = new PayOS({
+  clientId: process.env.PAYOS_CLIENT_ID,
+  apiKey: process.env.PAYOS_API_KEY,
+  checksumKey: process.env.PAYOS_CHECKSUM_KEY,
+});
 
 module.exports = payOs;
